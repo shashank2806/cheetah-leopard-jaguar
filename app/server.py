228b562ae16d56aa4c5719093279fce714c4e079
Shashank Shekhar <correspond.shashank@gmail.com>
2019-02-06 20:59:08 +0530

    Modify template

--- app/server.py
+++ app/server.py
@@ -8,10 +8,10 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1-7lcKZ0rxQScHEsQecnaSZwGV_pQEEVT'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['cheetah', 'leopard', 'jaguar']
 path = Path(__file__).parent
 
 app = Starlette()
